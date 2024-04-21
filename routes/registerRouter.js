5b84fc1856e0f1696a7d2c47d22a206f83d44bec
ChunHsinTung <k8795273@hotmail.com>
2024-04-21 22:07:51 +0800

    移除registerRouter不必要的console.log函式

--- routes/registerRouter.js
+++ routes/registerRouter.js
@@ -9,7 +9,6 @@ router.get('/', (req, res) => {
 })
 
 router.post('/', (req, res) => {
-    console.log(req.body)
     const newUser = new userModel({
         name: req.body.name,
         email: req.body.email,
